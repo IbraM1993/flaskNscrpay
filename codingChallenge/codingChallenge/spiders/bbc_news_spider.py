a6e4aaaf35f4fe40bef2f3291f35ad29098da87d
MAASSARANI Ibrahim <brt.imaassarani@cma-cgm.com>
2022-09-24 15:37:32 +0300

    Sucessfully defined the callback: changed parse_news_page_link to parse (Spider class requirements, otherwise, will cause bugs

--- codingChallenge/codingChallenge/spiders/bbc_news_spider.py
+++ codingChallenge/codingChallenge/spiders/bbc_news_spider.py
@@ -1,5 +1,4 @@
 import scrapy
-from scrapy.spiders import CrawlSpider
 
 class BbcNewsSpider(scrapy.Spider):
     name = "bbc_spider"
@@ -7,8 +6,15 @@ class BbcNewsSpider(scrapy.Spider):
 
     def parse(self, response):
         
-        news_link_class = "a.module__title__link.tag--news"
-        news_link = response.css(news_link_class).attrib['href']
-        news_link_text = response.css(f"{news_link_class}::text").get() # the text could be used as the name for the collection in MongoDB later on
-        
-        yield {"url": news_link, "url_text": news_link_text}
+        news_page_link_class = "a.module__title__link.tag--news"
+        news_page_link = response.css(news_page_link_class).attrib['href']
+        news_page_link_text = response.css(f"{news_page_link_class}::text").get() # the text could be used as the name of the collection in MongoDB later on
+
+        yield {"url": news_page_link, "url_text": news_page_link_text}
+
+        if news_page_link:
+            yield scrapy.Request(news_page_link, callback=self.parse_news)
+
+    def parse_news(self, response):
+        rsp = response.css("a.gs-c-promo-heading.gs-o-faux-block-link__overlay-link.gel-pica-bold.nw-o-link-split__anchor").extract_first()
+        yield {"response": rsp}
