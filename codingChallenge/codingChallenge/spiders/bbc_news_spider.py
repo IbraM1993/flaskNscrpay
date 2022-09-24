77bf34a7a694b705feed4057c66a716ed906fe38
MAASSARANI Ibrahim <brt.imaassarani@cma-cgm.com>
2022-09-24 16:10:47 +0300

    Added a test for response and another test and a TODO for the news page link

--- codingChallenge/codingChallenge/spiders/bbc_news_spider.py
+++ codingChallenge/codingChallenge/spiders/bbc_news_spider.py
@@ -4,15 +4,18 @@ class BbcNewsSpider(scrapy.Spider):
     name = "bbc_spider"
     start_urls = ["https://www.bbc.com/"]
 
+    # Spider class requirement: a function with the name "parse" must be defined
     def parse(self, response):
         
-        news_page_link_class = "a.module__title__link.tag--news"
-        news_page_link = response.css(news_page_link_class).attrib['href']
-        news_page_link_text = response.css(f"{news_page_link_class}::text").get() # the text could be used as the name of the collection in MongoDB later on
+        if response.status == 200: # if there is a response
+            news_page_link_class = "a.module__title__link.tag--news"
+            news_page_link = response.css(news_page_link_class).attrib['href']
 
-        yield {"url": news_page_link, "url_text": news_page_link_text}
+            # we could define a class for custom exceptions
+            #TODO check if css class for the target a tag exists first in the html response and throw the error there if it does not
+            if news_page_link is None:
+                raise Exception("News page link not found: check your code or if the css selection is no longer valid...")
 
-        if news_page_link:
             yield scrapy.Request(news_page_link, callback=self.parse_news)
 
     def parse_news(self, response):
