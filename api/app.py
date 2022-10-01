e98a10ae6e7dc727df582c53dab8a571b5e0215c
MAASSARANI Ibrahim <brt.imaassarani@cma-cgm.com>
2022-10-01 16:21:00 +0300

    Built the api to query DB based on keyword and added docs

---
+++ api/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
+import helpers as helpers
+
+app = Flask(__name__)
+
+MONGO_URI = "mongodb://localhost:27017/bbc"
+app.config["MONGO_URI"] = MONGO_URI
+
+connection = PyMongo(app)
+keyword = helpers.get_keyword_from_CLI()
+
+@app.route("/")
+def home():
+    """ Renders the home page which consists of two links: "News to all the news, and News by Keyword for the filtered news based on user input """
+    return render_template("home.html")
+
+@app.route("/news", methods=["GET"])
+def news():
+    """ Renders the news page which consists of all the news articles in the database """
+    return render_template("news.html", data=helpers.get_all_news_articles(connection))
+
+@app.route("/news_by_keyword", methods=["GET"])
+def news_by_keyword():
+    """ Renders the news by keyword page which consists of filtered news based on user input """
+    return render_template("news_by_keyword.html", data=helpers.get_news_articles_by_keyword(connection, keyword))
+
+if __name__ == '__main__':
+    app.run()
---
+++ api/helpers.py
@@ -0,0 +1,97 @@
+from flask_pymongo import PyMongo
+
+import json
+from bson import json_util
+
+import sys
+
+def get_keyword_from_CLI() -> str:
+    """
+    Takes no arguments and get the keyword from the list of input arguments to the CLI.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    keyword
+        the input search keyword from the user
+    """
+    args_list = sys.argv
+    i, nargs = 0, len(args_list)
+    keyword = ""
+    while i < nargs:
+        if args_list[i] == "--keyword":
+            i += 1
+
+            if i < len(args_list):
+                keyword = args_list[i]
+        
+        i += 1
+
+    return keyword
+
+def to_json(data) -> str:
+    """
+    Convert input data to JSON format.
+    
+    Parameters
+    ----------
+    data
+        the input data to be processed
+
+    Returns
+    -------
+    str
+        a JSON format of the input data
+    """
+    return json.dumps(data, default=json_util.default)
+
+def get_all_news_articles(connection: PyMongo) -> str:
+    """
+    Queries the database for all news.
+    
+    Parameters
+    ----------
+    connection
+        the MongoDB connection
+
+    Returns
+    -------
+    str
+        a JSON format of the data fetched from the queried MongoDB object
+    """
+    results = connection.db.news_2.find()
+    
+    news_articles = [ result for result in results ]
+    return to_json(news_articles)
+
+def get_news_articles_by_keyword(connection: PyMongo, keyword: str) -> str:
+    """
+    Queries the database for all news where the keyword is available in the article title or text.
+    
+    Parameters
+    ----------
+    connection
+        the MongoDB connection
+
+    keyword
+        the input keyword initially fetched from the CLI
+
+    Returns
+    -------
+    str
+        a JSON format of the data fetched from the queried MongoDB object
+    """
+    results = connection.db.news_2.find(
+        {
+            "$or": [
+                { "title": {"$regex": ".*" + keyword + ".*"} },
+                { "article_text": {"$regex": ".*" + keyword + ".*"} }
+            ]
+        }
+    )
+
+    news_articles = [ result for result in results ]
+    return to_json(news_articles)
